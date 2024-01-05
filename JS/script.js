28c30467e7a91dee30dc7e7eaf38203488dfec73
AlexNocua <nocua68@gmail.com>
2024-01-04 21:57:47 -0500

    Cambios na Estrutura da interface, tambem a agregacao de outra seccao de portafolio

--- JS/script.js
+++ JS/script.js
@@ -14,7 +14,25 @@ $(window).on("scroll", function () {
 
     } else {
         $(".navbar").removeClass("navbar-fixedremove");
-    }
+    };
 
 }
 )
+
+//implementacion de isotope
+
+$(document).ready(function () {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item'
+    });
+
+    $('#portfolio-filters li').on('click', function () {
+        $("#portfolio-filters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+
+        portfolioIsotope.isotope({
+            filter: $(this).data('filter')
+        })
+    });
+});
+
